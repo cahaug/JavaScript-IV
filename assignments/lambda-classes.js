eea909d40fa96663222f0e35cf4f3b0b93db0e23
cahaug <caleahaug@gmail.com>
2019-04-24 18:53:01 -0700

    built Instructor in lambda-classes.js

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -11,3 +11,18 @@ class Person {
         return `Hello my name is ${this.name}, I am from ${this.location}.`
     }
 }
+
+class Instructor extends Person {
+    constructor(props){
+        super(props);
+        this.specialty=props.specialty;
+        this.favLanguage=props.favLanguage;
+        this.catchPrase=props.catchPrase;
+    }
+    demo(subject){
+        return `Today we are learning about ${subject}.`
+    }
+    grade(student, subject){
+        return `${student.name} receives a perfect score on ${subject}.`
+    }
+}
